72df4b81d529e3a0b7f16e6edbc2d757c692eac5
annakassai <74617784+annakassai@users.noreply.github.com>
2021-02-02 23:31:04 +0100

    Added animated icons and styles

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactAnimatedWeather from 'react-animated-weather'
 import "./Forecast.css";
 
 export default function Forecast(props) {
@@ -16,7 +17,13 @@ export default function Forecast(props) {
         <div className="col-7">
           <h5 className="data">{weather.description}</h5>
           <h5 className="local-temperature">
-            <i className="fas fa-sun"></i>
+            <ReactAnimatedWeather
+              icon = 'CLEAR_DAY'
+              color = 'goldenrod'
+              size = {50}
+              animate = {true}
+              className="fasfasun"
+            />
             <span className="temperature">{weather.temperature}</span>
             <span className="units"> °C | °F </span>
           </h5>
@@ -24,16 +31,44 @@ export default function Forecast(props) {
           <h5 className="data">Humidity:{weather.humidity}%</h5>        
           <h5 className="data">Wind:{weather.wind}km/h</h5>
         </div>
-        <div className="col-2">
-          <p>10:00</p>
-          <p>13:00</p>
-          <p>16:00</p>
-          <p>19:00</p>
-          <p>22:00</p>
-          <p>24:00</p>
-        </div> 
         <div className="col-1">
-          <i className="fas fa-cloud-sun"></i>
+          <p>0:00</p>
+          <p>3:00</p>
+          <p>6:00</p>
+          <p>9:00</p>
+          <p>0:00</p>
+        </div> 
+        <div className="col-2">
+        <ReactAnimatedWeather
+          icon = 'CLEAR_DAY'
+          color = 'goldenrod'
+          size = {25}
+          animate = {true}
+        />
+         <ReactAnimatedWeather
+          icon = 'PARTLY_CLOUDY_DAY'
+          color = 'grey'
+          size = {25}
+          animate = {true}
+        />
+         <ReactAnimatedWeather
+          icon = 'CLOUDY'
+          color = 'darkblue'
+          size = {25}
+          animate = {true}
+        />
+        <ReactAnimatedWeather
+          icon = 'CLEAR_DAY'
+          color = 'goldenrod'
+          size = {25}
+          animate = {true}
+        />
+         <ReactAnimatedWeather
+          icon = 'PARTLY_CLOUDY_DAY'
+          color = 'grey'
+          size = {25}
+          animate = {true}
+        />
         </div>    
         <div className="col-1">
           <p>5°C</p>
@@ -41,7 +76,6 @@ export default function Forecast(props) {
           <p>2°C</p>
           <p>5°C</p>
           <p>4°C</p>
-          <p>2°C</p>
         </div>
       </div>
     </div>
